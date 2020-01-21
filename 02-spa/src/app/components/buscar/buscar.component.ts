83cd6a2f7e5a00a51c3e695d38fbebdfb667c454
troikap <30813351+troikap@users.noreply.github.com>
2020-01-21 08:42:15 -0300

    arreglando pedos

--- 02-spa/src/app/components/buscar/buscar.component.ts
+++ 02-spa/src/app/components/buscar/buscar.component.ts
@@ -1,26 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { HeroesService } from '../servicios/heroes.service';
-
+import { HeroesService } from '../../servicios/heroes.service';
 
 @Component({
-  selector: 'app-buscar',
-  templateUrl: './buscar.component.html'
+  selector: 'app-buscador',
+  templateUrl: './buscador.component.html'
 })
-export class BuscarComponent implements OnInit {
+export class BuscadorComponent implements OnInit {
 
-    heroes:any[] = [];
-    termino:string;
+  heroes:any[] = []
+  termino:string;
+
   constructor( private activatedRoute:ActivatedRoute,
-               private _heroesService:HeroesService) { }
+              private _heroesService: HeroesService) {
+
+  }
 
   ngOnInit() {
-    
-    this.activatedRoute.params.subscribe(params =>{ 
-      this.termino = params['termino'];
-      this.heroes = this._heroesService.buscarHeroe( params['termino'] );
-      console.log( this.heroes);})
-    
+
+    this.activatedRoute.params.subscribe( params =>{
+      this.termino =params['termino'];
+      this.heroes = this._heroesService.buscarHeroes( params['termino'] );
+      console.log( this.heroes );
+    });
+
   }
 
 }
